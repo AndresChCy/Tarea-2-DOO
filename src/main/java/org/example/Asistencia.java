513a38a66284799da89e8b6db80fea5902906679
AndresChCy <aichaparrom@gmail.com>
2024-05-12 01:27:29 -0400

    Metodos constructores para Asistencia y Retraso (Abierto a cambios)

--- src/main/java/org/example/Asistencia.java
+++ src/main/java/org/example/Asistencia.java
@@ -1,4 +1,8 @@
 package org.example;
 
 public class Asistencia {
+    private Empleado empleado
+    public Asistencia(Empleado empleado){
+        this.empleado = empleado ;
+    }
 }
--- src/main/java/org/example/Retraso.java
+++ src/main/java/org/example/Retraso.java
@@ -3,4 +3,8 @@ import java.time.Instant;
 
 public class Retraso extends Asistencia{
     private Instant hora;
+    public Retraso(Empleado empleado){
+        super(empleado);
+        hora = Instant.now();
+    }
 }
