e62224e0e02c0d91cd459d8121311c28b0e29d4a
AndresChCy <aichaparrom@gmail.com>
2024-05-14 20:57:50 -0400

    Base definida para invitacion

--- src/main/java/org/example/Invitacion.java
+++ src/main/java/org/example/Invitacion.java
@@ -1,6 +1,13 @@
 package org.example;
 import java.time.Instant;
+import java.util.List;
 
 public class Invitacion {
     private Instant hora;
+    private List<Invitable> invitados;
+
+    public Invitacion(List<Invitable> invitados, Instant hora){
+        this.invitados = invitados;
+        this.hora = hora;
+    }
 }
