bb1fdd05a7943959e7df9b90ed4ee964606bd675
Bastian Bernal <bbernal2022@udec.cl>
2024-05-15 13:07:57 -0400

    Implementación método constructor, getters y otros en clase Departamento

--- src/main/java/org/example/Departamento.java
+++ src/main/java/org/example/Departamento.java
@@ -4,6 +4,23 @@ import java.util.ArrayList;
 public class Departamento implements Invitable{
     private String nombre;
     private ArrayList<Empleado> empleados;
+    //Constructor
+    public Departamento(String nombre) {
+        this.nombre = nombre;
+        this.empleados = new ArrayList<>();
+    }
+    // Método getter
+    public String getNombre() {
+        return nombre;
+    }
+    // Método para agregar empleado al departamento
+    public void addEmpleado(Empleado empleado) {
+        empleados.add(empleado);
+    }
+    // Método para obtener la cantidad de empleados en el departamento
+    public int obtenerCantidadEmpleados() {
+        return empleados.size();
+    }
     public void invitar() {
         System.out.println("Invitando al departamento " + nombre);for (Empleado empleado : empleados) {
             empleado.invitar();
