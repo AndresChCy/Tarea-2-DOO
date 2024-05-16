f78b606d866c58384575b8066ad25f0de20ff334
AndresChCy <aichaparrom@gmail.com>
2024-05-15 22:31:12 -0400

    doc: implemetando documentación de Invitacion, Asistencia y Retraso

--- src/main/java/org/example/Retraso.java
+++ src/main/java/org/example/Retraso.java
@@ -1,8 +1,17 @@
 package org.example;
 import java.time.Instant;
 
+/**
+ * Clase para marcar la asistencia atrasada de un empleado a un evento
+ */
 public class Retraso extends Asistencia{
+    /** Hora a la que llego el empleado */
     private Instant hora;
+
+    /**
+     * Metodo constructor que asigna la hora actual de llegada y el empleado que esta llegando
+     * @param empleado el empleado que acaba de llegar
+     */
     public Retraso(Empleado empleado){
         super(empleado);
         hora = Instant.now();
